0d09d197cd20caf73f340f5a26596ef4e1160a06
Shaik Masroor Ahmed <masroor.sma@gmail.com>
2023-01-09 21:58:45 +0530

    first airbnb commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The project aims at recommending the airbnb customer vacation destination based on their demographics',
+    author='Shaik_Masroor_Ahmed',
+    license='',
+)
